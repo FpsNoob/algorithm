eac5b12194c65bc26ec215d5820a68d64a9af253
1393699349 <1393699349@qq.com>
2019-12-09 08:47:31 +0800

    update back track

--- src/include/BackTrack.h
+++ src/include/BackTrack.h
@@ -40,7 +40,102 @@ void graphColor(int m) {
 			k = k + 1;
 		}
 		else {
+			color[k] = 0;
 			k = k - 1;	//回溯到上一个结点
 		}
 	}
+}
+
+//批处理作业调度wenti
+int batchJob(int a[], int b[], int n) {
+	int i, k;
+	int x[10], sum1[10], sum2[10];
+	int best_time = 1000;
+	for (i = 1; i <= n; i++) {	//初始化调度方案
+		x[i] = -1;
+		sum1[i] = 0;
+		sum2[i] = 0;
+	}
+	sum1[0] = 0, sum2[0] = 0, k = 1;//调度第一个作业
+	while (k >= 1) {
+		x[k] = x[k] + 1;	//安排第k个作业，x[k]为作业编号
+		while (x[k] < n) {	//考察每一种作业
+			for (i = 1; i < k; i++) {	//检查作业x[k]是否重复
+				if (x[i] == x[k]) {
+					break;
+				}
+			}
+			if (i == k) {	//作业x[k]尚未处理
+				sum1[k] = sum1[k - 1] + a[x[k]];
+				if (sum1[k] > sum2[k - 1]) {
+					sum2[k] = sum1[k] + b[x[k]];
+				}
+				else {
+					sum2[k] = sum2[k - 1] + b[x[k]];
+				}
+				if (sum2[k] > best_time) {	//时间大于目前最短时间，剪枝
+					x[k] = x[k] + 1;	//调度下一个作业
+				}
+				else {
+					break;
+				}
+			}
+			else {
+				x[k] = x[k] + 1;	//x[k]已经被调度过，调度下一个作业
+			}
+		}
+		if (x[k] < n && k < n) {
+			k = k + 1;//安排下一个作业
+		}
+		else {
+			if (x[k] < n && k == n) {	//得到一个作业安排
+				if (best_time > sum2[k]) {
+					best_time = sum2[k];	//更换当前最短完成时间
+					cout << "目前的最短作业安排是：";
+					for (int j = 1; i <= n; j++) {
+						cout << x[j] + 1 << " ";	//作业编号从1开始
+					}
+					cout << "最短时间是：" << best_time << endl;
+				}
+				x[k] = -1;
+				k = k - 1;	//回溯
+			}
+		}
+	}
+	return best_time;
+}
+
+//n皇后问题
+int x[n] = {-1};	//皇后问题的解
+int Place(int k) {	//第k个皇后的位置，是否有冲突
+	for (int i = 0; i < k; i++) {
+		if (x[i] == x[k] || abs(i - k) == abs(x[i] - x[k])) {	//对角线，同列
+			return 1;
+		}
+		return 0;
+	}
+}
+
+void Queen(int n) {
+	int k = 1;
+	while (k >= 1) {	//摆放皇后k
+		x[k]++;
+		while (x[k] < n && Place(k) == 1) {	//有冲突
+			x[k]++;	//放到下一列
+		}
+		if (x[k] <= n && k == n) {	//得到一个皇后的摆放
+			for (int i = 0; i < n; i++) {
+				cout << x[i] + 1 << " ";
+			}
+			cout << endl;
+			return;
+		}
+		if (x[k] < n && k < n) {//尚有皇后未摆放
+			k = k + 1;//准备摆放下一个皇后
+		}
+		else {
+			x[k--] = -1;	//回溯
+		}
+	}
+	cout << "无解" << endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,70 @@
-#include "include/basic.h"
-#include "include/Chicken.h"
-#include "include/Search.h"
-#include "include/stringMatching.h"
-#include "include/Sort.h"
-#include "include/Divide.h"
+//#include "include/basic.h"
+//#include "include/Chicken.h"
+//#include "include/Search.h"
+//#include "include/stringMatching.h"
+//#include "include/Sort.h"
+//#include "include/Divide.h"
+//
+//int main()
+//{
+//	/*百元买鸡问题*/
+//	//Chicken();
+//
+//	/*顺序查找*/
+//	//int a[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	//int index = seqSearch(a, 10, 13);
+//	//if (index == 0) {
+//	//	cout << "找不到这个数。" << endl;
+//	//}
+//	//else {
+//	//	cout << "这个数的下标为：" << index << endl;
+//	//}
+//	//return 0;
+//
+//	/*串匹配-BF算法*/
+//	/*string s = "mofucker";
+//	string t = "fuck";
+//	int index = KMP(s, t);
+//	if (index == 0) {
+//		cout << "找不到这个串。" << endl;
+//	}
+//	else {
+//		cout << "这个串的下标开始为：" << index << endl;
+//	}*/
+//
+//	/*排序算法*/
+//	int a[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int len = 10;
+//	//selectionSort(a, len);
+//	//bubbleSort(a, len);
+//	//mergeSort(a,0, len-1);
+//	//quickSort(a, 0, len - 1);
+//	//for (int i = 0; i < len; i++) {
+//	//	cout << a[i] << " ";
+//	//}
+//
+//	//棋盘问题
+//	chessBoard(0, 0, 0, 1, 4);
+//	return 0;
+//}
 
-int main()
-{
-	/*百元买鸡问题*/
-	//Chicken();
+#include <stdio.h>
+#include <string.h>
 
-	/*顺序查找*/
-	//int a[10] = { 9,8,7,6,5,4,3,2,1,0 };
-	//int index = seqSearch(a, 10, 13);
-	//if (index == 0) {
-	//	cout << "找不到这个数。" << endl;
-	//}
-	//else {
-	//	cout << "这个数的下标为：" << index << endl;
-	//}
-	//return 0;
-
-	/*串匹配-BF算法*/
-	/*string s = "mofucker";
-	string t = "fuck";
-	int index = KMP(s, t);
-	if (index == 0) {
-		cout << "找不到这个串。" << endl;
+int main() {
+	char a[1000] = "123";
+	char b[1000];
+	int n, i, k, t, x;
+	n = strlen(a);
+	k = 0; t = 0;
+	for (i = n - 1; i >= 0; i--) {
+		x = (a[i] - '0') * 2 + t;
+		b[k] = x % 10;
+		t = x / 10;
+		k++;
 	}
-	else {
-		cout << "这个串的下标开始为：" << index << endl;
-	}*/
-
-	/*排序算法*/
-	int a[10] = { 9,8,7,6,5,4,3,2,1,0 };
-	int len = 10;
-	//selectionSort(a, len);
-	//bubbleSort(a, len);
-	//mergeSort(a,0, len-1);
-	//quickSort(a, 0, len - 1);
-	//for (int i = 0; i < len; i++) {
-	//	cout << a[i] << " ";
-	//}
-
-	//棋盘问题
-	chessBoard(0, 0, 0, 1, 4);
-	return 0;
+	for (int i = k-1; i >=0; i--) {
+		printf("%d", b[i]);
+	}
+	
 }
